53554beb7d0a1fccddbfeeddcc13a8ac141a0b12
llRizvanll <torizvan@gmail.com>
2023-10-03 19:07:52 +0530

    Day 4 find middle element in linkedlist

--- src/main/dsa/LinkedList/middle/MiddleElement.kt
+++ src/main/dsa/LinkedList/middle/MiddleElement.kt
@@ -1,4 +1,25 @@
 package LinkedList.middle
 
-class MiddleElement {
-}
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list. If there are two middle nodes, return the second middle node.
+ */
+
+/**
+ * Example:
+ * var li = LinkNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class LinkNode(var `val`: Int) {
+ *     var next: LinkNode? = null
+ * }
+ */
+    fun MiddleElement(node : LinkNode) : LinkNode? {
+        var p1 = node.head
+        var p2 = node.head
+
+        while (p2!= null && p2.next != null){
+            p1 = p1?.next;
+            p2 = p2?.next?.next;
+        }
+        return p1;
+    }
