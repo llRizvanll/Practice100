61d2b0ca2778bd75d2290ade9524eef841655def
llRizvanll <torizvan@gmail.com>
2023-10-07 20:54:15 +0530

    Day 7 reverse a linkedlist

---
+++ src/main/dsa/LinkedList/middle/other/ReverseLinkedList.java
@@ -0,0 +1,32 @@
+package LinkedList.middle.other;
+
+/**
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * Input: head = [1,2,3,4,5]
+ * Output: [5,4,3,2,1]
+ */
+public class ReverseLinkedList {
+
+    int val;
+    ReverseLinkedList next;
+    ReverseLinkedList(){}
+
+    ReverseLinkedList(int val){ this.val = val;}
+
+    ReverseLinkedList(int val, ReverseLinkedList node){
+        this.val= val;
+        this.next = node;
+    }
+
+    public ReverseLinkedList reverseList(ReverseLinkedList head) {
+        ReverseLinkedList newHead = null;
+        while (head!=null){
+            ReverseLinkedList next = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = next;
+        }
+        return newHead;
+    }
+
+}
