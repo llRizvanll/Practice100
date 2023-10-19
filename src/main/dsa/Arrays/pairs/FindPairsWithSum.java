56224feaa5afc5edf29956a841495e397057df08
llRizvanll <torizvan@gmail.com>
2023-10-19 22:47:04 +0530

    Day 12 find all pairs with a given sum

--- src/main/dsa/Arrays/pairs/FindPairsWithSum.java
+++ src/main/dsa/Arrays/pairs/FindPairsWithSum.java
@@ -1,2 +1,66 @@
-package Arrays.pairs;public class FindPairsWithSum {
+package Arrays.pairs;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/***
+ * https://www.geeksforgeeks.org/print-all-pairs-with-given-sum/
+ *
+ * Given an array of integers, and a number ‘sum’, print all pairs in the array whose sum is equal to ‘sum’.
+ *
+ * Examples :
+ * Input  :  arr[] = {1, 5, 7, -1, 5},
+ *           sum = 6
+ * Output : (1, 5) (7, -1) (1, 5)
+ * Input  :  arr[] = {2, 5, 17, -1},
+ *           sum = 7
+ * Output :  (2, 5)
+ */
+public class FindPairsWithSum {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 3, 1, 5, 6, 7};
+        int targetSum = 8;
+
+        List<Pair> pairs = findPairsInSum(arr,targetSum);
+        for (Pair pair : pairs) {
+            System.out.println("(" + pair.getFirst() + ", " + pair.getSecond() + ")");
+        }
+    }
+
+    private static List<Pair> findPairsInSum(int[] arr, int targetSum) {
+        Map<Integer,Integer> map = new HashMap<>();
+        ArrayList<Pair> pairList = new ArrayList<>();
+
+        for (int num :
+                arr) {
+            int sumValue = targetSum - num;
+            if(map.containsKey(sumValue)){
+                pairList.add(new Pair(num,sumValue));
+            }
+            map.put(num,1);
+        }
+
+        return pairList;
+    }
+
+    static class Pair {
+        private final int first;
+        private final int second;
+
+        public Pair(int first,int second){
+            this.first = first;
+            this.second = second;
+        }
+
+        public int getFirst() {
+            return first;
+        }
+
+        public int getSecond() {
+            return second;
+        }
+    }
 }
--- src/main/dsa/Arrays/pairs/FindPiarsWithSum.kt
+++ src/main/dsa/Arrays/pairs/FindPiarsWithSum.kt
@@ -1,2 +1,41 @@
 package Arrays.pairs
 
+/***
+ * https://www.geeksforgeeks.org/print-all-pairs-with-given-sum/
+ *
+ * Given an array of integers, and a number ‘sum’, print all pairs in the array whose sum is equal to ‘sum’.
+ *
+ * Examples :
+ * Input  :  arr[] = {1, 5, 7, -1, 5},
+ *           sum = 6
+ * Output : (1, 5) (7, -1) (1, 5)
+ * Input  :  arr[] = {2, 5, 17, -1},
+ *           sum = 7
+ * Output :  (2, 5)
+ */
+class Pair(val first: Int, val second: Int)
+
+fun findPairsWithSum(arr: IntArray, targetSum: Int): List<Pair> {
+    val pairs = ArrayList<Pair>()
+    val seen = HashMap<Int, Int>()
+
+    for (num in arr) {
+        val complement = targetSum - num
+        if (seen.containsKey(complement)) {
+            pairs.add(Pair(num, complement))
+        }
+        seen[num] = 1
+    }
+
+    return pairs
+}
+
+fun main() {
+    val arr = intArrayOf(2, 4, 3, 1, 5, 6, 7)
+    val targetSum = 8
+    val result = findPairsWithSum(arr, targetSum)
+
+    for (pair in result) {
+        println("(${pair.first}, ${pair.second})")
+    }
+}
