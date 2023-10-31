13615e30a21d2eb477388e3b799e98d39c9c69f4
llRizvanll <torizvan@gmail.com>
2023-10-31 10:29:43 +0530

    Day 19 Post order  traversal

---
+++ src/main/dsa/Tree/root/traversal/PostOrderTraversal.java
@@ -0,0 +1,39 @@
+package Tree.root.traversal;
+
+class PostOrderNode {
+    int data;
+
+    PostOrderNode left, right;
+
+    public PostOrderNode(int _data){
+        data = _data;
+        left = right = null;
+    }
+}
+public class PostOrderTraversal {
+
+    PostOrderNode root;
+
+    void getPostOrderTraversal(PostOrderNode node){
+        if (node == null) return;
+
+        getPostOrderTraversal(node.left);
+        getPostOrderTraversal(node.right);
+
+        System.out.println("data => "+ node.data);
+    }
+
+    public static void main(String[] args) {
+        PostOrderTraversal postOrderTraversal = new PostOrderTraversal();
+
+        postOrderTraversal.root = new PostOrderNode(1);
+        postOrderTraversal.root.left = new PostOrderNode(2);
+        postOrderTraversal.root.right = new PostOrderNode(3);
+        postOrderTraversal.root.left.left = new PostOrderNode(4);
+        postOrderTraversal.root.left.right = new PostOrderNode(5);
+
+        System.out.println("Post order traversal ");
+
+        postOrderTraversal.getPostOrderTraversal(postOrderTraversal.root);
+    }
+}
